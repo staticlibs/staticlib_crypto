97a89bde47c042adbf0e450d6ad67ec5e8ec1ab9
alex <alex@staticlibs.net>
2017-04-10 20:45:26 +0100

    0.5.2 docs

--- docs/html/search/all_77.js
+++ docs/html/search/all_77.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['write',['write',['../classstaticlib_1_1crypto_1_1sha256__sink.html#a069714aac38d6fd0f26b3205c9e3044d',1,'staticlib::crypto::sha256_sink']]]
+  ['write',['write',['../classstaticlib_1_1crypto_1_1sha256__sink.html#a82698e866d7df32a1d23a23c80182500',1,'staticlib::crypto::sha256_sink']]]
 ];
--- docs/html/search/functions_72.js
+++ docs/html/search/functions_72.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['read',['read',['../classstaticlib_1_1crypto_1_1digest__sign__source.html#ac5c9ffbc132d0eb9f77d6608cf4df025',1,'staticlib::crypto::digest_sign_source::read()'],['../classstaticlib_1_1crypto_1_1digest__verify__source.html#a3b6f1cb30f0cd84fce15b2649f0fa6a2',1,'staticlib::crypto::digest_verify_source::read()'],['../classstaticlib_1_1crypto_1_1sha256__source.html#adb88e6692e182e24e1314976ad7590df',1,'staticlib::crypto::sha256_source::read()']]]
+  ['read',['read',['../classstaticlib_1_1crypto_1_1digest__sign__source.html#a9e179e2fb293237f4d4581cbedeefde4',1,'staticlib::crypto::digest_sign_source::read()'],['../classstaticlib_1_1crypto_1_1digest__verify__source.html#ae30bc4a5ae5c5e913ce6c038a441b737',1,'staticlib::crypto::digest_verify_source::read()'],['../classstaticlib_1_1crypto_1_1sha256__source.html#a69ff83295d774145cdfb542121b468f1',1,'staticlib::crypto::sha256_source::read()']]]
 ];
